4ca67ca2ccb1720c8e5e20ef9bb99fce008a8030
Ash1062 <ashishdevindra@gmail.com>
2024-08-02 07:48:27 +0000

    New add up

---
+++ 01_js/one.js
@@ -0,0 +1,11 @@
+console.log(Math.floor(Math.random() * 10) + 1);
+let date = new Date();
+console.log(date.toLocaleString('default', {
+    weekday: "long"
+}));
+const krishna = ["Madhu", "Mangal", "Balram"];
+const shiva = ["Nandi", "Ganesh", "Uma"];
+const god = krishna.concat(shiva);
+console.log(god);
+
+
